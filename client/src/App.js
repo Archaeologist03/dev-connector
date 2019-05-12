02f005fbd96bdc30effbee586586aee49d9a9f85
Archaeologist03 <omniiknight@hotmail.com>
2019-05-12 16:23:01 +0200

    Protected Route for Dashborad.

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,9 @@ import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 import './App.css';
 
 if (localStorage.token) {
@@ -38,6 +41,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>

840ccccd1588b56e66921d1d936a881094ce0a6d
Archaeologist03 <omniiknight@hotmail.com>
2019-05-13 15:25:10 +0200

    Add Education & Experience.

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,10 @@ import Register from './components/auth/Register';
 
 import Dashboard from './components/dashboard/Dashboard';
 
-import CreateProfile from './components/profile-form/CreateProfile';
-import EditProfile from './components/profile-form/EditProfile';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 import './App.css';
 
@@ -55,6 +57,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
